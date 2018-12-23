2c5f06f49324461ebf7d8613cbced3dcb8d39bee
Haeun Kim <ggrace.kim93@gmail.com>
2018-12-23 17:59:45 +0900

    mocketl: Remove MockEtlDatabaseConfigException.java

--- src/main/java/com/example/mocketl/exceptions/MockEtlDatabaseConfigException.java
+++
@@ -1,9 +0,0 @@
-package com.example.mocketl.exceptions;
-
-public class MockEtlDatabaseConfigException extends Exception {
-    private static final String MSG = "Database config is invalid";
-
-    public MockEtlDatabaseConfigException() {
-        super(MSG);
-    }
-}
